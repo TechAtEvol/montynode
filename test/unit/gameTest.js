d996717ac0ec2316ebcb1589b5b910e7e07cc152
evolstaffan <staffan@evol.se>
2018-01-05 20:31:09 +0100

    more tasks

--- test/unit/gameTest.js
+++ test/unit/gameTest.js
@@ -2,5 +2,8 @@ describe('a mounty hall game',() => {
   it('has a running test', (done) => {
     done();
   });
-  //it can setup a find Nessie game with 3 lakes and a random placed Nessie beneath 
+  //it can setup a find Nessie game with 3 lakes and a random placed Nessie beneath
+  //it can take a selected lake and present one lake without Nessie
+  //it can can present a found or not found on a lake switch for the game
+  //it can store and present the result over a simulation of a given number of games
 });
